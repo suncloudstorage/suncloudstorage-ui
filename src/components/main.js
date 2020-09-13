7041b227a4cb92fa4894953c4524b362b0ca61f7
suncloudstorage <suncloudstorage@gmail.com>
2020-09-13 19:12:50 +0300

    created main components

--- src/components/main.js
+++ src/components/main.js
@@ -1,23 +1,15 @@
 import React, {Component} from "react";
 import userService from "../services/userService";
+import saveAs  from 'file-saver';
 
 class Main extends Component {
-
     downloadFile() {
         userService.downloadFile().then(response => {
-            // var bytes = new Uint8Array(response.data); // pass your byte response to this constructor
-            //
-            // var blob=new Blob([bytes], {type: response.headers.contentType});// change resultByte to bytes
-            //
-            // var link=document.createElement('a');
-            // link.href=window.URL.createObjectURL(blob);
-            // link.download="person-icon.png";
-            // link.click();
-
-            let base64ToArrayBuffer = userService.base64ToArrayBuffer(response.data);
-            userService.createAndDownloadBlobFile(base64ToArrayBuffer, 'person-icon', 'png' )
+            let url = response.config.url;
+            saveAs(response.data, url.slice(url.lastIndexOf("/") + 1));
         })
     }
+
 
     render() {
         return (
--- src/services/userService.js
+++ src/services/userService.js
@@ -3,6 +3,7 @@ import axios from "axios";
 class UserService {
     baseUrl = 'http://localhost:8080';
     requestHeader = {
+        responseType: 'blob',
         headers: {
             'Access-Control-Allow-Origin': '*',
             'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
@@ -14,41 +15,8 @@ class UserService {
         return axios.post(`${this.baseUrl}/api/v1/auth/login`, {username, password})
     }
 
-    base64ToArrayBuffer(base64: string) {
-        // const binaryString = window.atob(base64); // Comment this if not using base64
-        const bytes = new Uint8Array(base64.length);
-        return bytes.map((byte, i) => base64.charCodeAt(i));
-    }
-
-    createAndDownloadBlobFile(body, filename, extension = 'pdf') {
-        const blob = new Blob([body]);
-        const fileName = `${filename}.${extension}`;
-        if (navigator.msSaveBlob) {
-            // IE 10+
-            navigator.msSaveBlob(blob, fileName);
-        } else {
-            const link = document.createElement('a');
-            // Browsers that support HTML5 download attribute
-            if (link.download !== undefined) {
-                const url = URL.createObjectURL(blob);
-                link.setAttribute('href', url);
-                link.setAttribute('download', fileName);
-                link.style.visibility = 'hidden';
-                document.body.appendChild(link);
-                link.click();
-                document.body.removeChild(link);
-            }
-        }
-    }
-
-    downloadFile() {
-        // return axios({
-        //     method: 'get',
-        //     url: `${this.baseUrl}/storage/downloadFile`,
-        //     params: {url: "s3://sunadmin/person-icon.png"},
-        //     header: this.headers
-        // });
-        return axios.get(`${this.baseUrl}/storage/downloadFile?url=s3://sunadmin/person-icon.png`, this.requestHeader);
+    downloadFile = () => {
+        return  axios.get(`${this.baseUrl}/storage/downloadFile?url=s3://sunadmin/person-icon.png`, this.requestHeader);
     }
 }
 
